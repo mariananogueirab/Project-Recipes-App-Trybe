b9a82bdffdc20e7294ea4c800a9c3f3c9e160914
mariananogueirab <mariananbp@gmail.com>
2021-09-21 18:29:10 -0300

    Requisitos 3 e 4

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
 import Input from '../components/Input';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
       <Input
         type="email"
         testid="email-input"
-        /* value={}
-        onChange={} */
+        value={ email }
+        onChange={ ({ target }) => setEmail(target.value) }
       />
 
       <Input
         type="text"
         testid="password-input"
-        /* value={}
-        onChange={} */
+        value={ password }
+        onChange={ ({ target }) => setPassword(target.value) }
       />
 
       <Button testid="login-submit-btn" />
