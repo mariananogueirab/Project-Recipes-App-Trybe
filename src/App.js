c720575d84b4a439fe74b23977c955dd91cbed8b
mariananogueirab <mariananbp@gmail.com>
2021-09-21 17:30:13 -0300

    Estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import RecipesProvider from './context/RecipesProvider';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <RecipesProvider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        {/* <Route path="/comidas" component={ Foods } />
+      <Route exact path="/bebidas" component={ Drinks } />
+      <Route exact path="/comidas/:id" component={ FoodsDetails } />
+      <Route exact path="/bebidas/:id" component={ DrinksDetails } />
+      <Route exact path="/comidas/:id/in-progress" component={ FoodInProgress } />
+      <Route exact path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
+      <Route exact path="/explorar" component={ Explore } />
+      <Route exact path="/explorar/comidas" component={ ExploreFoods } />
+      <Route exact path="/explorar/bebidas" component={ ExploreDrinks } />
+      <Route exact path="/explorar/comidas/ingredientes" component={ ExploreFoodsIngredients } />
+      <Route exact path="/explorar/bebidas/ingredientes" component={ ExploreDrinksIngredients } />
+      <Route exact path="/explorar/comidas/area" component={ ExploreFoodsArea } />
+      <Route exact path="/perfil" component={ Profile } />
+      <Route exact path="/receitas-feitas" component={ RecipesMade } />
+      <Route exact path="/receitas-favoritas" component={ FavoritesRecipes } /> */}
+      </Switch>
+    </RecipesProvider>
   );
 }
 
