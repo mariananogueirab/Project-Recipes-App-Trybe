da68defb9aebe1548a96fb7371b1b0473a6df74c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-09-22 19:35:46 -0300

    Requirements 47, 48, 49

---
+++ src/pages/DrinkInProgress.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function DrinkInProgress() {
+  const ingredients = ['Ingr 1', 'Ingr 2', 'Ingr 3'];
+
+  const ingredientsList = ingredients.map((ingredient, index) => (
+    <li key={ ingredient } data-testid={ `${index}-ingredient-step}` }>
+      <input type="checkbox" name={ ingredient } />
+    </li>
+  ));
+
+  return (
+    <div>
+      <img src="inserir" alt="imagem do prato" data-testid="recipe-photo" />
+      {/* Criar um componente RecipeHeader */}
+      <div>
+        <h2 data-testid="recipe-title">Titulo da Receita</h2>
+        <button type="button" data-testid="share-btn">Compartilhar Receita</button>
+        <button type="button" data-testid="favorite-btn">Favoritar Receita</button>
+        <h3 data-testid="recipe-category">Categoria da Receita</h3>
+      </div>
+      {/* Criar um Componente Ingredients */}
+      <div>
+        <h3>Ingredients</h3>
+        <ul>
+          { ingredientsList }
+        </ul>
+      </div>
+      {/* Criar um componente Instructions */}
+      <div data-testid="instructions">
+        {/* texto com as instruções vem aqui */}
+        <section>
+          texto com as instruções da receita
+        </section>
+      </div>
+      <button type="button" data-testid="finish-recipe-btn">
+        Finalizar Receita
+      </button>
+    </div>
+  );
+}
+
+export default DrinkInProgress;
