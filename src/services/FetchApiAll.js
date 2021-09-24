644ba4c7fe8b11018fd88678e98fc355d542e8e5
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-09-24 09:40:59 -0300

    refatora implementaçao do componente RecipesCardFood

--- src/services/FetchApiAll.js
+++ src/services/FetchApiAll.js
@@ -5,23 +5,17 @@ const API_FOODS_RECOMENDATION = 'https://www.themealdb.com/api/json/v1/1/search.
 const API_FOOD_BY_ID = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 const API_DRINKS_RECOMENDATION = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const getApiByAllDrinks = () => (
-  fetch(API_BY_ALL_DRINKS)
-    .then((response) => (
-      response
-        .json()
-        .then((data) => (response.ok ? Promise.resolve(data) : Promise.reject(data)))
-    ))
-);
+export const getApiByAllDrinks = async () => {
+  const { drinks } = await fetch(API_BY_ALL_DRINKS)
+    .then((res) => res.json());
+  return drinks;
+};
 
-export const getApiByAllFoods = () => (
-  fetch(API_BY_ALL_FOODS)
-    .then((resp) => (
-      resp
-        .json()
-        .then((data) => (resp.ok ? Promise.resolve(data) : Promise.reject(data)))
-    ))
-);
+export const getApiByAllFoods = async () => {
+  const { meals } = await fetch(API_BY_ALL_FOODS)
+    .then((res) => res.json());
+  return meals;
+};
 
 export const getDrinkById = async (id) => {
   const { drinks } = await fetch(`${API_DRINK_BY_ID}${id}`)
