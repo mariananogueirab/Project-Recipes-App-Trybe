02442940938d9a164d6a07bde44fc469643b4e3f
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-09-24 18:38:31 -0300

    Implementa Tela de explorar bebidas ou comidas

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ testid, disabled, onClick, label }) {
+function Button({ testid, disabled, onClick, label, style }) {
   return (
     <button
       type="submit"
       data-testid={ testid }
       disabled={ disabled }
       onClick={ onClick }
+      style={ style }
     >
       {label}
     </button>
@@ -20,11 +21,13 @@ Button.propTypes = {
   label: PropTypes.string.isRequired,
   disabled: PropTypes.bool,
   onClick: PropTypes.func,
+  style: PropTypes.objectOf(String),
 };
 
 Button.defaultProps = {
   disabled: undefined,
   onClick: undefined,
+  style: {},
 };
 
 export default Button;
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ExploreDrinkFood from '../components/ExploreDrinkFood';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,7 @@ function ExploreFoods() {
   return (
     <div>
       <Header pageTitle="Explorar Comidas" hasSearchIcon={ false } />
+      <ExploreDrinkFood />
       <Footer />
     </div>
   );
