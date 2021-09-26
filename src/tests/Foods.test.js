102033629285744f3cecde215c272ccb44a98aaf
Haron Prestes <haronprestes1303@gmail.com>
2021-09-26 18:58:53 -0300

    Teste H2 foods.test

---
+++ src/tests/Foods.test.js
@@ -0,0 +1,18 @@
+import { fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './utils/utils';
+
+describe('Teste Foods.jsx', () => {
+  test('verifica se ao entrar na página contém um h2 com o name "Comidas"', () => {
+    const { history } = renderWithRouter(
+      <App />,
+    );
+    history.push('/comidas');
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: /Comidas/,
+    })).toBeInTheDocument();
+  });
+});
