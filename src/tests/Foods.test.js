d4f10b28889408307ccde12034feb7ff242e9310
Haron Prestes <haronprestes1303@gmail.com>
2021-09-27 19:03:25 -0300

    Correçoes de lint

--- src/tests/Foods.test.js
+++ src/tests/Foods.test.js
@@ -1,5 +1,4 @@
-import { fireEvent, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './utils/utils';
