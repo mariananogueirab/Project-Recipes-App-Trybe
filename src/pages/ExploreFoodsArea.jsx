b996c072ddd1fe9dc5d5825ebe457b58d54fdb0a
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-09-27 23:28:25 -0300

    inicia implementaçao da tela de explorar ingredientes por area

--- src/pages/ExploreFoodsArea.jsx
+++ src/pages/ExploreFoodsArea.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { getFoodByArea } from '../services/FetchApiAll';
 
 function ExploreFoodsArea() {
+  const [area, setArea] = useState([]);
+
+  useEffect(() => {
+    async function getListFoodByArea() {
+      const meals = await getFoodByArea();
+      setArea(meals);
+    }
+    getListFoodByArea();
+  }, []);
+
   return (
     <div>
       <Header pageTitle="Explorar Origem" hasSearchIcon="active" />
+      <select
+        data-testid="explore-by-area-dropdown"
+      >
+        {
+          area.map(({ strArea }, index) => (
+            <option key={ index } data-testid={ `${strArea}-option` }>{ strArea }</option>
+          ))
+        }
+      </select>
       <Footer />
     </div>
   );
